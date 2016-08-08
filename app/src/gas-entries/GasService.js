6d2ea8a3c006ee825d06fcd3d92aad3d82560fd4
Oscar Barba <oscarb@mty.ipointsystems.com>
2016-08-08 17:42:55 -0500

    Fixed first entry bug

--- app/src/gas-entries/GasService.js
+++ app/src/gas-entries/GasService.js
@@ -31,17 +31,19 @@
         var data = $localStorage.entries;
         var lastEntry = $localStorage.lastEntry;
 
-        entry.date = new Date().toISOString();
-        // Save days since last entry
-        var timeDiff = Math.abs(new Date(entry.date).getTime() - new Date(lastEntry.date).getTime());
-        entry.daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        if(!!lastEntry) {
+          entry.date = new Date().toISOString();
+          // Save days since last entry
+          var timeDiff = Math.abs(new Date(entry.date).getTime() - new Date(lastEntry.date).getTime());
+          entry.daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-        //Save kms since last entry.
-        var kmsDiff = entry.kms - lastEntry.kms;
-        var lastElement = data.length - 1;
-        data[lastElement].kmDiffNext = kmsDiff;
-        //data[lastElement].gasPerformance = kmsDiff / data[lastElement].l;
-        data.kmDiffPrevious = kmsDiff;
+          //Save kms since last entry.
+          var kmsDiff = entry.kms - lastEntry.kms;
+          var lastElement = data.length - 1;
+          data[lastElement].kmDiffNext = kmsDiff;
+          //data[lastElement].gasPerformance = kmsDiff / data[lastElement].l;
+          data.kmDiffPrevious = kmsDiff;
+        }
 
         data.push(entry);
         $localStorage.lastEntry = entry;
