ce4bb4f0be215c37a7c12b8247e173c0f27d7a2f
Oscar Barba <oscarb@mty.ipointsystems.com>
2016-08-08 16:33:56 -0500

    Added gas performance history

--- app/src/gas-entries/GasService.js
+++ app/src/gas-entries/GasService.js
@@ -29,7 +29,20 @@
       },
       saveEntry: function(entry) {
         var data = $localStorage.entries;
+        var lastEntry = $localStorage.lastEntry;
+
         entry.date = new Date().toISOString();
+        // Save days since last entry
+        var timeDiff = Math.abs(new Date(entry.date).getTime() - new Date(lastEntry.date).getTime());
+        entry.daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+        //Save kms since last entry.
+        var kmsDiff = entry.kms - lastEntry.kms;
+        var lastElement = data.length - 1;
+        data[lastElement].kmDiffNext = kmsDiff;
+        //data[lastElement].gasPerformance = kmsDiff / data[lastElement].l;
+        data.kmDiffPrevious = kmsDiff;
+
         data.push(entry);
         $localStorage.lastEntry = entry;
         $localStorage.entries = data;
